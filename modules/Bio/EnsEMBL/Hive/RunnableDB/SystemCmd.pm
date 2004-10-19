3decbe7b3ae14234caaa4deda29f253c52bc7b7a
Jessica Severin <jessica@ebi.ac.uk>
2004-10-19 14:52:59 +0000

    added simple RunnableDB which that the input_id and runs it as a system call. All STDOUT and STDERR from the command are autoamtically captured and redirected to files (locations stored in analysis_job_file table). Very simple idea, but might prove useful.

---
+++ modules/Bio/EnsEMBL/Hive/RunnableDB/SystemCmd.pm
@@ -0,0 +1,80 @@
+#
+# You may distribute this module under the same terms as perl itself
+#
+# POD documentation - main docs before the code
+
+=pod 
+
+=head1 NAME
+
+Bio::EnsEMBL::Hive::RunnableDB::SystemCmd
+
+=cut
+
+=head1 SYNOPSIS
+
+my $db      = Bio::EnsEMBL::DBAdaptor->new($locator);
+my $runDB   = Bio::EnsEMBL::Hive::RunnableDB::SystemCmd->new ( 
+                                                    -db      => $db,
+                                                    -input_id   => $input_id
+                                                    -analysis   => $analysis );
+$runDB->fetch_input(); #reads from DB
+$runDB->run();
+$runDB->output();
+$runDB->write_output(); #writes to DB
+
+=cut
+
+=head1 DESCRIPTION
+
+This object is a very simple module.  It takes the input_id
+and runs it as a system command.
+
+=cut
+
+=head1 CONTACT
+
+jessica@ebi.ac.uk
+
+=cut
+
+=head1 APPENDIX
+
+The rest of the documentation details each of the object methods. 
+Internal methods are usually preceded with a _
+
+=cut
+
+package Bio::EnsEMBL::Hive::RunnableDB::SystemCmd;
+
+use strict;
+
+use Bio::EnsEMBL::Pipeline::RunnableDB;
+our @ISA = qw(Bio::EnsEMBL::Pipeline::RunnableDB);
+
+
+##############################################################
+#
+# override inherited fetch_input, run, write_output methods
+# so that nothing is done
+#
+##############################################################
+
+sub fetch_input {
+  my $self = shift;
+  return 1;
+}
+
+sub run
+{
+  my $self = shift;
+  system($self->input_id);
+  return 1;
+}
+
+sub write_output {
+  my $self = shift;
+  return 1;
+}
+
+1;
