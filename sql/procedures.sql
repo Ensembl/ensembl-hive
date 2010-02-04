d229396076021fef1c4f3624f75fb812da836b8f
Leo Gordon <lg4@ebi.ac.uk>
2010-02-04 11:04:02 +0000

    add some useful server-side procedures to the hive

---
+++ sql/procedures.sql
@@ -0,0 +1,51 @@
+##########################################################################################
+#
+# Some stored functions and procedures used in hive:
+#
+
+############ make it more convenient to convert logic_name into analysis_id: #############
+
+DROP FUNCTION IF EXISTS analysis_name2id;
+DELIMITER |
+CREATE FUNCTION analysis_name2id(param_logic_name CHAR(64))
+RETURNS INT
+DETERMINISTIC
+BEGIN
+    DECLARE var_analysis_id INT;
+    SELECT analysis_id INTO var_analysis_id FROM analysis WHERE logic_name=param_logic_name;
+    RETURN var_analysis_id;
+END
+|
+DELIMITER ;
+
+
+############## show hive progress for all analyses: #######################################
+
+DROP PROCEDURE IF EXISTS show_progress;
+CREATE PROCEDURE show_progress()
+    SELECT a.logic_name, j.status, j.retry_count, count(*)
+    FROM analysis_job j, analysis a
+    WHERE a.analysis_id=j.analysis_id
+    GROUP BY a.analysis_id, j.status, j.retry_count
+    ORDER BY a.analysis_id, j.status;
+
+############## show hive progress for a particular analysis (given by name) ###############
+
+DROP PROCEDURE IF EXISTS show_progress_analysis;
+CREATE PROCEDURE show_progress_analysis(IN param_logic_name char(64))
+    SELECT a.logic_name, j.status, j.retry_count, count(*)
+    FROM analysis_job j, analysis a
+    WHERE a.analysis_id=j.analysis_id
+    AND   a.logic_name=param_logic_name
+    GROUP BY j.status, j.retry_count;
+
+############## reset failed jobs for analysis #############################################
+
+DROP PROCEDURE IF EXISTS reset_failed_jobs_for_analysis;
+CREATE PROCEDURE reset_failed_jobs_for_analysis(IN param_logic_name char(64))
+    UPDATE analysis_job j, analysis a
+    SET j.status='READY', j.retry_count=0, j.job_claim=''
+    WHERE a.logic_name=param_logic_name
+    AND   a.analysis_id=j.analysis_id
+    AND   j.status='FAILED';
+
