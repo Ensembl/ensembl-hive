0e8d1cad1dc0f210c9797b0cc0b79f85e378b52c
Leo Gordon <lg4@ebi.ac.uk>
2010-06-29 08:45:41 +0000

    do not seem to be used (and we have other ways to do the same things)

--- sql/find_work.sql
+++
@@ -1,1 +0,0 @@
-select logic_name, analysis.analysis_id, status, count(*) from analysis_job, analysis where analysis_job.analysis_id=analysis.analysis_id and status='READY' group by analysis_job.analysis_id;
--- sql/hive-status.sql
+++
@@ -1,2 +0,0 @@
-select analysis.analysis_id,logic_name, status, count(*) from analysis_job, analysis where analysis_job.analysis_id=analysis.analysis_id group by analysis_job.analysis_id, status;
-
