4ec364e5d4fa81cc43913d18c84cd26546191053
Leo Gordon <lg4@ebi.ac.uk>
2011-01-14 11:02:43 +0000

    try turning meta_value into TEXT

--- sql/patch_2011-01-14.sql
+++ sql/patch_2011-01-14.sql
@@ -1,6 +1,13 @@
 # Extend several fields in analysis to 255 characters:
 
-ALTER TABLE analysis     MODIFY COLUMN module varchar(255);
-ALTER TABLE db_file      MODIFY COLUMN module varchar(255);
-ALTER TABLE program      MODIFY COLUMN module varchar(255);
-ALTER TABLE program_file MODIFY COLUMN module varchar(255);
+ALTER TABLE analysis     MODIFY COLUMN module VARCHAR(255);
+ALTER TABLE db_file      MODIFY COLUMN module VARCHAR(255);
+ALTER TABLE program      MODIFY COLUMN module VARCHAR(255);
+ALTER TABLE program_file MODIFY COLUMN module VARCHAR(255);
+
+ALTER TABLE meta DROP INDEX species_key_value_idx;
+ALTER TABLE meta DROP INDEX species_value_idx;
+ALTER TABLE meta MODIFY COLUMN meta_value TEXT;
+ALTER TABLE meta ADD UNIQUE KEY species_key_value_idx (species_id,meta_key,meta_value(255));
+ALTER TABLE meta ADD        KEY species_value_idx (species_id,meta_value(255));
+
