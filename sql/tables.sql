ea0d027112f42d08b99e9681f2ee5d9f9f4c037a
Leo Gordon <lg4@ebi.ac.uk>
2011-01-19 11:39:33 +0000

    extending the meta value to TEXT

--- sql/tables.sql
+++ sql/tables.sql
@@ -12,11 +12,11 @@ CREATE TABLE IF NOT EXISTS meta (
   meta_id                     INT NOT NULL AUTO_INCREMENT,
   species_id                  INT UNSIGNED DEFAULT 1,
   meta_key                    VARCHAR(40) NOT NULL,
-  meta_value                  VARCHAR(255) BINARY NOT NULL,
+  meta_value                  TEXT NOT NULL,
 
   PRIMARY   KEY (meta_id),
-  UNIQUE    KEY species_key_value_idx (species_id, meta_key, meta_value),
-            KEY species_value_idx (species_id, meta_value)
+  UNIQUE    KEY species_key_value_idx (species_id, meta_key, meta_value(255)),
+            KEY species_value_idx (species_id, meta_value(255))
 
 ) COLLATE=latin1_swedish_ci ENGINE=MyISAM;
 
