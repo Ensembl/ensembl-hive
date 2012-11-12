2d429085994b3da95411d2354ab8a492e291bec0
Leo Gordon <lg4@ebi.ac.uk>
2012-11-12 14:15:40 +0000

    updated the release number to 70 in the schema

--- sql/tables.sql
+++ sql/tables.sql
@@ -421,5 +421,5 @@ CREATE TABLE monitor (
 
 
 # Auto add schema version to database (should be overridden by Compara's table.sql)
-INSERT IGNORE INTO meta (species_id, meta_key, meta_value) VALUES (NULL, 'schema_version', '69');
+INSERT IGNORE INTO meta (species_id, meta_key, meta_value) VALUES (NULL, 'schema_version', '70');
 
