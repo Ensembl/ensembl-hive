756de99dad3245ca0072836c7744846890008da7
Kathryn Beal <kbeal@ebi.ac.uk>
2013-02-07 11:42:11 +0000

    Updated to release 71

--- sql/tables.sql
+++ sql/tables.sql
@@ -423,5 +423,5 @@ CREATE TABLE monitor (
 
 
 # Auto add schema version to database (should be overridden by Compara's table.sql)
-INSERT IGNORE INTO meta (species_id, meta_key, meta_value) VALUES (NULL, 'schema_version', '70');
+INSERT IGNORE INTO meta (species_id, meta_key, meta_value) VALUES (NULL, 'schema_version', '71');
 
