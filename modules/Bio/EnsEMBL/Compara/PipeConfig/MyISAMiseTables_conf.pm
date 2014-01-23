b204efe0f183e4168bc38673c50b3e99df7b141f
Matthieu Muffato <muffato@ebi.ac.uk>
2014-01-23 12:00:22 +0000

    Personal emails are forbidden

--- modules/Bio/EnsEMBL/Compara/PipeConfig/MyISAMiseTables_conf.pm
+++ modules/Bio/EnsEMBL/Compara/PipeConfig/MyISAMiseTables_conf.pm
@@ -33,7 +33,11 @@ limitations under the License.
 
 =head1 CONTACT
 
-  Please contact ehive-users@ebi.ac.uk mailing list with questions/suggestions.
+Please email comments or questions to the public Ensembl
+developers list at <http://lists.ensembl.org/mailman/listinfo/dev>.
+
+Questions may also be sent to the Ensembl help desk at
+<http://www.ensembl.org/Help/Contact>.
 
 =cut
 
