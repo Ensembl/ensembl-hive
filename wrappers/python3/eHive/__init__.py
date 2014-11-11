0e771760793f1ac29c743607ff2f07714b2adbac
Matthieu Muffato <muffato@ebi.ac.uk>
2014-11-11 00:23:04 +0000

    Exceptions are also exported

--- wrappers/python3/eHive/__init__.py
+++ wrappers/python3/eHive/__init__.py
@@ -1,6 +1,5 @@
 
-import eHive.Process
-from eHive.Process import BaseRunnable
-
-__all__ = [ 'BaseRunnable' ]
+# We take all the interesting classes from both modules, i.e. BaseRunnable and all the exceptions
+from eHive.Process import CompleteEarlyException, BaseRunnable
+from eHive.Params import ParamException, ParamNameException, ParamSubstitutionException, ParamInfiniteLoopException
 
