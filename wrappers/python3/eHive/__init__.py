ed304b882abdc1095ea84bc312c3b9edd3a38193
Matthieu Muffato <muffato@ebi.ac.uk>
2014-11-13 17:41:03 +0000

    ParamWarning should also be exported

--- wrappers/python3/eHive/__init__.py
+++ wrappers/python3/eHive/__init__.py
@@ -1,6 +1,6 @@
 
 # We take all the interesting classes from both modules, i.e. BaseRunnable and all the exceptions
 from eHive.Process import BaseRunnable, CompleteEarlyException, JobFailedException
-from eHive.Params import ParamException, ParamNameException, ParamSubstitutionException, ParamInfiniteLoopException
+from eHive.Params import ParamException, ParamNameException, ParamSubstitutionException, ParamInfiniteLoopException, ParamWarning
 
-__all__ = ['BaseRunnable', 'CompleteEarlyException', 'JobFailedException', 'ParamException', 'ParamNameException', 'ParamSubstitutionException', 'ParamInfiniteLoopException']
+__all__ = ['BaseRunnable', 'CompleteEarlyException', 'JobFailedException', 'ParamException', 'ParamNameException', 'ParamSubstitutionException', 'ParamInfiniteLoopException', 'ParamWarning']
