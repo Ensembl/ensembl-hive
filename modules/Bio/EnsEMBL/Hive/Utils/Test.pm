bd1acb7090f7f1e0e646fe27f8a60e7e874703b6
Matthieu Muffato <muffato@ebi.ac.uk>
2015-01-27 10:37:02 +0000

    Only redefine locally methods in Bio::EnsEMBL::Hive::Process (to allow the later implementation of runWorker to use them)

--- modules/Bio/EnsEMBL/Hive/Utils/Test.pm
+++ modules/Bio/EnsEMBL/Hive/Utils/Test.pm
@@ -43,45 +43,39 @@ sub spurt {
 }
 
 
-my $events_to_test = undef;
 sub standaloneJob {
     my ($module_or_file, $param_hash, $expected_events, $flags) = @_;
 
-    $events_to_test = $expected_events ? [@$expected_events] : undef;
+    my $events_to_test = $expected_events ? [@$expected_events] : undef;
 
     my $input_id = stringify($param_hash);
 
-    eval {
-        ok(Bio::EnsEMBL::Hive::Scripts::StandaloneJob::standaloneJob($module_or_file, $input_id, $flags, undef, 1), 'job completed');
+    my $_test_event = sub {
+        if (@$events_to_test) {
+            is_deeply([@_], (shift @$events_to_test), "$_[0] event");
+        } else {
+            fail("event-stack is empty, cannot get the next expected event");
+            use Data::Dumper;
+            print Dumper([@_]);
+        }
     };
-    if ($@) {
-        fail(sprintf('standaloneJob("%s", "%s")', $module_or_file, stringify($param_hash)));
-        print $@, "\n";
-    }
+
+    local *Bio::EnsEMBL::Hive::Process::dataflow_output_id = sub {
+        shift;
+        &$_test_event('DATAFLOW', @_);
+        return [1];
+    } if $expected_events;
+
+    local *Bio::EnsEMBL::Hive::Process::warning = sub {
+        shift;
+        &$_test_event('WARNING', @_);
+    } if $expected_events;
+
+    lives_and(sub {
+        ok(Bio::EnsEMBL::Hive::Scripts::StandaloneJob::standaloneJob($module_or_file, $input_id, $flags, undef, 1), 'job completed');
+    }, sprintf('standaloneJob("%s", %s, (...), %s)', $module_or_file, stringify($param_hash), stringify($flags)));
+
     ok(!scalar(@$events_to_test), 'no untriggered events') if $expected_events;
-}
-
-
-*Bio::EnsEMBL::Hive::Process::dataflow_output_id = sub {
-    shift;
-    _test_event('DATAFLOW', @_);
-    return [1];
-};
-
-*Bio::EnsEMBL::Hive::Process::warning = sub {
-    shift;
-    _test_event('WARNING', @_);
-};
-
-sub _test_event {
-    return unless $events_to_test;
-    if (@$events_to_test) {
-        is_deeply([@_], (shift @$events_to_test), "$_[0] event");
-    } else {
-        fail("event-stack is empty, cannot get the next expected event");
-        use Data::Dumper;
-        print Dumper([@_]);
-    }
 }
 
 
