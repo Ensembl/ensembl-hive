65ca04b4e181a156062511398139f9190f2ab172
Matthieu Muffato <muffato@ebi.ac.uk>
2015-05-15 08:36:15 +0100

    Added a new "05.runnabledb" test for python

--- wrappers/python3/TestRunnable.py
+++ wrappers/python3/TestRunnable.py
@@ -2,23 +2,27 @@
 import eHive
 
 class TestRunnable(eHive.BaseRunnable):
-    """Simple Runnable to test standaloneJob"""
+    """Simple Runnable to test as a standaloneJob"""
 
     def param_defaults(self):
         return {
-            'pdef' : 78
+            'alpha' : 37,
+            'beta' : 78
         }
 
     def fetch_input(self):
         self.warning("Fetch the world !")
-        print("alpha is", self.param('alpha'))
-        self.param('new_param', 999)
+        print("alpha is", self.param_required('alpha'))
+        print("beta is", self.param_required('beta'))
 
     def run(self):
         self.warning("Run the world !")
-        print("beta is", self.param_required('beta'))
-        print("new_param is", self.param('new_param'))
+        s = self.param('alpha') + self.param('beta')
+        print("set gamma to", s)
+        self.param('gamma', s)
 
     def write_output(self):
         self.warning("Write to the world !")
         print("gamma is", self.param('gamma'))
+        self.dataflow( {'gamma': self.param('gamma')}, 2 )
+
