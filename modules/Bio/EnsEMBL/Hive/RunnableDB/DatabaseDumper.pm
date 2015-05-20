f66a8e1de4c0c5742040c5fae20c7a490dfcf9bb
Matthieu Muffato <muffato@ebi.ac.uk>
2015-05-20 18:07:37 +0100

    Standalone jobs have job_id=-1, and "-" is not allowed in parameter names

--- modules/Bio/EnsEMBL/Hive/RunnableDB/DatabaseDumper.pm
+++ modules/Bio/EnsEMBL/Hive/RunnableDB/DatabaseDumper.pm
@@ -197,7 +197,7 @@ sub run {
     # Check whether the current database has been restored from a snapshot.
     # If it is the case, we shouldn't re-dump and overwrite the file.
     # We also check here the value of the "skip_dump" parameter
-    my $completion_signature = sprintf('dump_%d_restored', $self->input_job->dbID);
+    my $completion_signature = sprintf('dump_%d_restored', $self->input_job->dbID < 0 ? 0 : $self->input_job->dbID);
     return if $self->param('skip_dump') or $self->param($completion_signature);
 
     # OK, we can dump
