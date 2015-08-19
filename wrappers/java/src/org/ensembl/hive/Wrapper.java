59e7118e0ff6b46086a4193fe12c32ec3834cad2
Dan Staines <dstaines@ebi.ac.uk>
2015-08-19 12:34:02 +0100

    control debug from wrapper

--- wrappers/java/src/org/ensembl/hive/Wrapper.java
+++ wrappers/java/src/org/ensembl/hive/Wrapper.java
@@ -16,15 +16,33 @@ public class Wrapper {
 
 	public static void main(String[] args) throws Exception {
 
-		System.setProperty(org.slf4j.impl.SimpleLogger.DEFAULT_LOG_LEVEL_KEY,
-				"TRACE");
 
-		Logger log = LoggerFactory.getLogger(Wrapper.class);
-
-		if (args.length != 3) {
-			log.error("Usage: java org.ensembl.hive.Wrapper <Runnable class> <input pipe> <output pipe>");
+		if (args.length != 4) {
+			System.err.println("Usage: java org.ensembl.hive.Wrapper <Runnable class> <input pipe> <output pipe> <debug>");
 			System.exit(1);
 		}
+
+		// set the debug level accordingly
+		switch(args[3]) {
+			case "0":
+				System.setProperty(org.slf4j.impl.SimpleLogger.DEFAULT_LOG_LEVEL_KEY,
+						"INFO");
+				break;
+			case "1":
+				System.setProperty(org.slf4j.impl.SimpleLogger.DEFAULT_LOG_LEVEL_KEY,
+						"DEBUG");
+				break;
+			case "2":
+				System.setProperty(org.slf4j.impl.SimpleLogger.DEFAULT_LOG_LEVEL_KEY,
+						"TRACE");
+				break;
+			default:
+				System.setProperty(org.slf4j.impl.SimpleLogger.DEFAULT_LOG_LEVEL_KEY,
+						"WARN");
+				break;
+		}
+
+		Logger log = LoggerFactory.getLogger(Wrapper.class);
 		log.info("Instantiating runnable module " + args[0]);
 		Class<?> clazz = Class.forName(args[0]);
 		if (!BaseRunnable.class.isAssignableFrom(clazz)) {
