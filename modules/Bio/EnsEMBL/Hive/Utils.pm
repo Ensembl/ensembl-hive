9d7edf37f92b47bc90b1da02f8599b853d896425
Leo Gordon <lg4@ebi.ac.uk>
2016-03-10 19:12:26 +0000

    bugfix: make sure the outer pair of quotes is removed as per bash rules (using a new function) -- thanks to Harpreet for reporting

--- modules/Bio/EnsEMBL/Hive/Utils.pm
+++ modules/Bio/EnsEMBL/Hive/Utils.pm
@@ -59,7 +59,7 @@ use Bio::EnsEMBL::Hive::DBSQL::SqlSchemaAdaptor;
 use Bio::EnsEMBL::Hive::DBSQL::DBConnection;
 
 use Exporter 'import';
-our @EXPORT_OK = qw(stringify destringify dir_revhash parse_cmdline_options find_submodules load_file_or_module script_usage url2dbconn_hash go_figure_dbc report_versions);
+our @EXPORT_OK = qw(stringify destringify dir_revhash parse_cmdline_options find_submodules load_file_or_module script_usage split_for_bash url2dbconn_hash go_figure_dbc report_versions);
 
 
 =head2 stringify
@@ -269,6 +269,32 @@ sub script_usage {
         <main::DATA>;   # this is just to stop the 'used once' warnings
     }
     exit($retvalue);
+}
+
+
+=head2 split_for_bash
+
+    Description: This function takes one argument (String) and splits it assuming it represents bash command line parameters.
+                 It mainly splits on whitespace, except for cases when spaces are trapped between quotes or apostrophes.
+                 In the latter case the outer quotes are removed.
+    Returntype : list of Strings
+
+=cut
+
+sub split_for_bash {
+    my $cmd = pop @_;
+
+    my @cmd = ($cmd =~ /((?:".*?"|'.*?'|\S)+)/g);   # split on space except for quoted strings
+
+    foreach my $syll (@cmd) {                       # remove the outer quotes or apostrophes
+        if($syll=~/^(\S*?)"(.*?)"(\S*?)$/) {
+            $syll = $1 . $2 . $3;
+        } elsif($syll=~/^(\S*?)'(.*?)'(\S*?)$/) {
+            $syll = $1 . $2 . $3;
+        }
+    }
+
+    return @cmd;
 }
 
 
