c5e4fff6959988b64db955dde2d5b2c41cff0570
ens-bwalts <bwalts@ebi.ac.uk>
2016-04-27 10:47:13 +0100

    Added new kmer example PipeConfigs, Runnables, and tests

---
+++ modules/Bio/EnsEMBL/Hive/Examples/Kmer/RunnableDB/SplitSequence.pm
@@ -0,0 +1,232 @@
+=pod 
+
+=head1 NAME
+
+   Bio::EnsEMBL::Hive::Examples::Kmer::RunnableDB::SplitSequence
+
+=head1 SYNOPSIS
+
+    Please refer to Bio::EnsEMBL::Hive::Examples::Kmer::PipeConfig::Kmer_conf pipeline configuration file
+    to understand how this particular example pipeline is configured and ran.
+
+=head1 DESCRIPTION
+
+    'Bio::EnsEMBL::Hive::Examples::Kmer::RunnableDB::SplitSequence is a utility runnable to split a DNA sequence into several shorter, 
+    overlapping subsequences. (Compare Bio::EnsEMBL::Hive::RunnableDB::FastaFactory, which splits a file containing many sequences
+    into several files containing some of the sequences in the original file, but keeps the sequences intact).
+
+    As input, it takes the name of a file with one or more DNA sequences,
+    the format of that file (which must be supported by Bio::SeqIO), 
+    a chunk size in base-pairs (BP),
+    and an overlap size, also in BP.
+
+    Overlap size must be >= 1. Chunk size must be > overlap size.
+
+    From this, it generates subsequences [chunk-size] BP long, overlapping each other by [overlap-size] BP.
+    The last chunk may be shorter than [chunk-size] BP, but will always be at least [overlap-size] + 1 BP
+
+    It then stores each of these chunks into new FASTA-format files, one chunk per file.
+
+    It flows out each chunk's filename, along with a flag indicating if a particular chunk is the last chunk
+    from the original sequence.
+
+
+=head1 LICENSE
+
+    Copyright [1999-2016] Wellcome Trust Sanger Institute and the EMBL-European Bioinformatics Institute
+
+    Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+         http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software distributed under the License
+    is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and limitations under the License.
+
+=head1 CONTACT
+
+    Please subscribe to the Hive mailing list:  http://listserver.ebi.ac.uk/mailman/listinfo/ehive-users  to discuss Hive-related questions or to be notified of our updates
+
+=cut
+
+package Bio::EnsEMBL::Hive::Examples::Kmer::RunnableDB::SplitSequence;
+
+use strict;
+use warnings;
+
+use Bio::SeqIO;
+use Bio::Seq;
+
+use Data::Dumper;
+
+use base ('Bio::EnsEMBL::Hive::Process');
+
+=head2 param_defaults
+
+    Description : Implements param_defaults() interface method of Bio::EnsEMBL::Hive::Process that defines module defaults for parameters.
+
+=cut
+
+sub param_defaults {
+
+  return {
+	  'output_prefix'     => 'my_chunk_',
+	  'output_suffix'     => '.fasta',
+	  'input_format'      => 'FASTA',
+	 };
+}
+
+=head2 fetch_input
+
+    Description : Implements fetch_input() interface method of Bio::EnsEMBL::Hive::Process that is used to read in parameters and load data.
+
+    There are no hard and fast rules on whether to fetch parameters in fetch_input(), or to wait until run() to fetch them.
+    In general, fetch_input() is a place to validate parameter existence and values for errors before the worker get set into RUN state
+    from the FETCH_INPUT state. 
+
+    
+
+=cut
+
+sub fetch_input {
+
+}
+
+=head2 run
+
+    Description : Implements run() interface method of Bio::EnsEMBL::Hive::Process that is used to perform the main bulk of the job (minus input and output).
+
+    Here, run() reads in the filename of a file containing a DNA sequence, an integer chunk-size in bases,
+    and an integer overlap size in bases. The file needs to be in a format supported by Bio::SeqIO.
+
+    param('inputfile'): Name of the file containing the sequence to split
+    param('input_format'):  Format of the sequence file (e.g. FASTA)
+    param('chunk_size'):    Desired chunk size in base-pairs
+    param('overlap_size'):  Desired length of overlap between chunks, in base-pairs
+
+
+
+=cut
+
+sub run {
+  my $self = shift @_;
+  
+  my $inputfile = $self->param_required('inputfile');
+  my $chunk_size = $self->param_required('chunk_size');
+  my $overlap_size = $self->param_required('overlap_size');
+
+  my $input_seqio;
+  if ( $inputfile =~ /\.(?:gz|Z)$/) {
+    open(my $in_fh, '-|', "gunzip -c $inputfile");
+    $input_seqio = Bio::SeqIO->new(-fh => $in_fh, -format => $self->param('input_format'));
+    $self->param('input_fh', $in_fh); # storing as a param so that it can be closed by post_cleanup if necessary 
+  } else {
+    $input_seqio = Bio::SeqIO->new(-file => $inputfile, -format => $self->param('input_format'));
+  }
+  die "Could not open or parse '$inputfile', please investigate" unless $input_seqio;
+
+
+  if (($chunk_size < 1) ||
+      ($overlap_size < 0) ||
+      ($overlap_size >= $chunk_size)) {
+    die "chunk_size must be > overlap_size, and both must be positive";
+  }
+
+  my $seq = $input_seqio->next_seq()->seq();
+  my $split_sequences = _split_sequence($seq, $chunk_size, $overlap_size);
+  $self->param('split_sequences', $split_sequences);
+
+}
+
+=head2 write_output
+
+    Description: Implements the write_output() interface method of Bio::EnsEMBL::Hive::Process that is used to flow output to the rest of the pipeline.
+
+    Here, we flow out three values:
+    * chunk_name    -- the name of a file containing a sequence chunk
+    * is_last_chunk -- a flag indicating if this is the last chunk of the chunked sequence
+    * output_format -- the format of the output file. For this runnable, this is hard-coded as 'FASTA'
+                       as it always outputs FASTA format files
+
+=cut
+
+
+sub write_output {
+  my $self = shift @_;
+
+  my $is_last_chunk = 0;
+
+  my @split_sequences = @{$self->param('split_sequences')};
+
+  for (my $i = 0; $i <= $#split_sequences; $i++) {
+    my $chunk_filename = $self->param('output_prefix') . $i . $self->param('output_suffix');
+    my $chunk_seqio = Bio::SeqIO->new(-file => '>' . $chunk_filename,
+				      -format => 'fasta');
+    my $seq_object = Bio::Seq->new(-seq => $split_sequences[$i],
+				   -id => $chunk_filename);
+    $chunk_seqio->write_seq($seq_object);
+
+    if ($i == $#split_sequences) {
+      $is_last_chunk = 1;
+    }
+
+    $self->dataflow_output_id( {'chunk_name' => $chunk_filename,
+				'is_last_chunk' => $is_last_chunk,
+				'output_format' => 'FASTA',
+			       }, 2);
+  }
+
+}
+
+=head2 post_cleanup
+
+    Description: Here, we implement the post_cleanup method of Bio::EnsEMBL::Hive::Process that is used to take care
+of housekeeping details after a runnable finishes.
+
+    In this case, it closes the input filehandle, as it is no longer needed
+
+=cut
+
+sub post_cleanup {
+  my $self = shift;
+  close( $self->param('input_fh') ) if $self->param('input_fh');
+}
+
+=head2 _split_sequence
+
+    Description: This is a private function (not a method, so it doesn't know about parameters) 
+    that takes a long sequence and returns a collection of shorter subsequences.
+
+    Arg [1] : The sequence, as a string
+    Arg [2] : The length of each subsequence (if possible). Subsequences will be no longer than
+              this value, but one may be shorter
+    Arg [3] : Number of bases to overlap adjacent subsequences
+
+=cut
+
+sub _split_sequence {
+  my ($seq, $chunk_size, $overlap_size) = @_;
+
+  my @split_sequences;
+
+  my @seqarr = split (//, $seq); # substring operations more efficient if string is put into an array
+
+  my $chunk_pointer = 0;
+  my $last_chunk = 0;
+  do {
+    my $chunk_end = ($chunk_pointer + $chunk_size) - 1;
+    if ($chunk_end >= $#seqarr) {
+      $chunk_end = $#seqarr;
+      $last_chunk = 1;
+    }
+    my $subseq = join("", @seqarr[$chunk_pointer..$chunk_end]);
+    push(@split_sequences, $subseq);
+    
+    $chunk_pointer = $chunk_end - $overlap_size;
+  } while ($last_chunk == 0);
+
+  return \@split_sequences;
+}
+
+1;
---
+++ t/10.pipeconfig/kmer.t
@@ -0,0 +1,143 @@
+#!/usr/bin/env perl
+
+# Copyright [1999-2016] Wellcome Trust Sanger Institute and the EMBL-European Bioinformatics Institute
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#      http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+use strict;
+use warnings;
+
+use Test::More;
+use Data::Dumper;
+use File::Temp qw{tempdir};
+
+use Bio::EnsEMBL::Hive::Utils::Test qw(init_pipeline runWorker);
+
+# eHive needs this to initialize the pipeline (and run db_cmd.pl)
+$ENV{'EHIVE_ROOT_DIR'} = File::Basename::dirname( File::Basename::dirname( File::Basename::dirname( Cwd::realpath($0) ) ) );
+
+# Fasta file for calculating kmers in long sequence mode
+my $inputfasta = File::Basename::dirname( File::Basename::dirname( Cwd::realpath($0) ) ).'/input_fasta.fa';
+# Fastq file for calculating kmers in short sequence mode
+my $inputfastq = File::Basename::dirname( File::Basename::dirname( Cwd::realpath($0) ) ).'/input_fastq.fastq';
+
+
+my $dir = tempdir CLEANUP => 1;
+my $original = chdir $dir;
+
+my $base_ehive_test_pipeline_url = $ENV{'EHIVE_TEST_PIPELINE_URLS'} || 'sqlite:///ehive_test_pipeline_db';
+my $ehive_test_pipeconfigs   = $ENV{'EHIVE_TEST_PIPECONFIGS'} || qw(KmerPipeline_conf KmerPipelineAoH_conf); 
+
+my $kmer_pipeline_modes      = 'short long';
+my $kmer_param_configs       = {'short' => [-seqtype => "short",
+					    -inputfile => "$inputfastq",
+					    -chunk_size => 40,
+					    -output_prefix => "k_split_",
+					    -output_suffix => ".fastq",
+					    -input_format => "FASTQ",
+					    -k => 5],
+				'long' => [-seqtype => "long",
+					   -inputfile => "$inputfasta",
+					   -chunk_size => 40,
+					   -output_prefix => "k_split_",
+					   -output_suffix => ".fa",
+					   -input_format => "FASTA",
+					   -k => 5]
+			       };
+
+
+
+my @pipeline_cfgs = split( /[\s,]+/, $ehive_test_pipeconfigs ) ;
+my @kmer_pipeline_modes = split( /[\s,]+/, $kmer_pipeline_modes ) ;
+
+foreach my $kmer_version ( @pipeline_cfgs ) {
+
+  foreach my $kmer_pipeline_mode ( @kmer_pipeline_modes ) {
+    my $pipeline_url = $base_ehive_test_pipeline_url . "_" . $kmer_pipeline_mode;
+    warn "\nInitializing the $kmer_version $kmer_pipeline_mode sequence pipeline into $pipeline_url ...\n\n";
+
+    my $pipeline_options = [@{$kmer_param_configs->{$kmer_pipeline_mode}}, -pipeline_url => $pipeline_url, -hive_force_init => 1,];
+
+    my $url              = init_pipeline('Bio::EnsEMBL::Hive::Examples::Kmer::PipeConfig::'.$kmer_version, $pipeline_options );
+
+    my $pipeline = Bio::EnsEMBL::Hive::HivePipeline->new(
+							 -url                        => $url,
+							 -disconnect_when_inactive   => 1,
+							);
+
+    # override the 'take_time' PipelineWideParameter in the loaded HivePipeline object to make the internal test Worker run quicker:
+
+    # First run a single worker in this process
+    runWorker($pipeline, { can_respecialize => 1 });
+    
+    my $hive_dba    = $pipeline->hive_dba;
+    my $job_adaptor = $hive_dba->get_AnalysisJobAdaptor;
+    is(scalar(@{$job_adaptor->fetch_all("status != 'DONE'")}), 0, 'All the runWorker jobs could be run');
+    
+    # Let's now try the combination of end-user scripts: seed_pipeline + beekeeper
+    {
+      # override the 'take_time' PipelineWideParameter directly in the database to make the external test Workers run quicker:
+      $hive_dba->get_PipelineWideParametersAdaptor->update( {'param_name' => 'take_time', 'param_value' => 0} );
+      
+      my @beekeeper_cmd = ($ENV{'EHIVE_ROOT_DIR'}.'/scripts/beekeeper.pl', -url => $hive_dba->dbc->url, -sleep => 0.02, '-loop', '-local');
+      
+      system(@beekeeper_cmd);
+      ok(!$?, 'beekeeper exited with the return code 0');
+      is(scalar(@{$job_adaptor->fetch_all("status != 'DONE'")}), 0, 'All the jobs could be run');
+    }
+    
+    my $final_result_nta = $hive_dba->get_NakedTableAdaptor( 'table_name' => 'final_result' );
+    my $final_results = $final_result_nta->fetch_all();
+
+    if ($kmer_pipeline_mode eq 'long') {
+      is(scalar(@$final_results), 66, 'There are exactly 66 final_results');
+      
+      my $sum_of_spotchecks = 0;
+      foreach ( @$final_results ) {
+	if ($_->{'kmer'} eq 'ACGAT') {
+	  $sum_of_spotchecks += $_->{'frequency'};
+	}
+	if ($_->{'kmer'} eq 'ACGTA') {
+	  $sum_of_spotchecks += $_->{'frequency'};
+	}
+	
+      }
+      ok( 16 == $sum_of_spotchecks, # if last kmer isn't correctly discarded, this will be 17
+	  sprintf("f(ACGAT) + f(ACGTA)=%f", , $sum_of_spotchecks) );
+    }
+
+    if ($kmer_pipeline_mode eq 'short') {
+      is(scalar(@$final_results), 407, 'There are exactly 407 final_results');
+
+      my $sum_of_spotchecks = 0;
+      foreach ( @$final_results ) {
+	if ($_->{'kmer'} eq 'AGCGC') {
+	  $sum_of_spotchecks += $_->{'frequency'};
+	}
+	if ($_->{'kmer'} eq 'ATGAT') {
+	  $sum_of_spotchecks += $_->{'frequency'};
+	}
+      }
+      ok( 5 == $sum_of_spotchecks,
+	  sprintf("f(AGCGC) + f(ATGAT)=%f", , $sum_of_spotchecks) );
+    }
+#    system( @{ $hive_dba->dbc->to_cmd(undef, undef, undef, 'DROP DATABASE') } );
+  }
+}
+
+
+done_testing();
+
+chdir $original;
+
