f1c2d86ec43c5c82ad928604ecf6b58d40248ba1
Leo Gordon <lg4@ebi.ac.uk>
2017-02-09 00:19:49 +0000

    multiple improvements of visualize_jobs, including support for new add_node()

--- scripts/visualize_jobs.pl
+++ scripts/visualize_jobs.pl
@@ -103,25 +103,39 @@ my %job_node_hash = ();
 sub add_job_node {
     my $job = shift @_;
 
-    my $job_node_name   = 'job_'.$job->dbID;
+    my $job_id          = $job->dbID;
+    my $job_node_name   = 'job_'.$job_id;
 
     unless($job_node_hash{$job_node_name}++) {
-        my $job_shape           = 'Mrecord';
-        my $job_status_colour   = {'DONE' => 'DeepSkyBlue', 'READY' => 'green', 'SEMAPHORED' => 'orange', 'FAILED' => 'red'}->{$job->status} // 'blue';
+        my $job_shape           = 'record';
+        my $job_status_colour   = {'DONE' => 'DeepSkyBlue', 'READY' => 'green', 'SEMAPHORED' => 'grey', 'FAILED' => 'red'}->{$job->status} // 'blue';
         my $job_id              = $job->dbID;
         my $job_params          = destringify($job->input_id);
 
         my $job_label           = qq{<<table border="0" cellborder="0" cellspacing="0" cellpadding="1">}
-                                 .qq{<tr><td bgcolor="grey">job_id:</td><td bgcolor="grey">$job_id</td></tr>};
-        while(my ($param_key, $param_value) = each %$job_params) {
+                                 .qq{<tr><td><u><i>job_id:</i></u></td><td><i>$job_id</i></td></tr>};
+
+        if(my $param_id_stack = $job->param_id_stack) {
+            $job_label  .=  qq{<tr><td><u><i>params from:</i></u></td><td><i>$param_id_stack</i></td></tr>};
+        }
+
+        foreach my $param_key (sort keys %$job_params) {
+            my $param_value = $job_params->{$param_key};
             $job_label  .= "<tr><td>$param_key:</td><td> $param_value</td></tr>";
         }
+
+        my $accu_adaptor    = $job->adaptor->db->get_AccumulatorAdaptor;
+        my $accu            = $accu_adaptor->fetch_structures_for_job_ids( $job_id )->{ $job_id };
+
+        foreach my $accu_name (sort keys %$accu) {
+            $job_label  .=  qq{<tr><td><u><i>accumulated:</i></u></td><td><i>$accu_name</i></td></tr>};
+        }
+
         $job_label  .= "</table>>";
 
 
         $self->{'graph'}->add_node( $job_node_name,
-            shape       => 'record',
-            comment     => qq{new_shape:$job_shape},
+            shape       => $job_shape,
             style       => 'filled',
             fillcolor   => $job_status_colour,
             label       => $job_label,
@@ -141,15 +155,20 @@ sub add_semaphore_node {
     my $semaphore = shift @_;
 
     my $semaphore_node_name         = 'semaphore_'.$semaphore->dbID;
-    my $semaphore_label             = $semaphore->local_jobs_counter .' + '. $semaphore->remote_jobs_counter;
-    my $semaphore_status_colour     = ($semaphore_label eq '0 + 0') ? 'darkgreen' : 'purple';
-    my $dependent_blocking_colour   = ($semaphore_label eq '0 + 0') ? 'grey' : 'red';
+    my $semaphore_blockers          = $semaphore->local_jobs_counter + $semaphore->remote_jobs_counter;
+    my $semaphore_is_blocked        = $semaphore_blockers > 0;
+
+    my ($semaphore_colour, $semaphore_shape, $dependent_blocking_arrow_colour, $dependent_blocking_arrow_shape ) = $semaphore_is_blocked
+        ? ('red', 'triangle', 'red', 'tee')
+        : ('darkgreen', 'invtriangle', 'darkgreen', 'none');
+
+    my $semaphore_label = $semaphore_is_blocked ? 'blockers: '.$semaphore_blockers : 'open';
 
     unless($semaphore_node_hash{$semaphore_node_name}++) {
         $self->{'graph'}->add_node( $semaphore_node_name,
-            shape       => 'trapezium',
+            shape       => $semaphore_shape,
             style       => 'filled',
-            fillcolor   => $semaphore_status_colour,
+            fillcolor   => $semaphore_colour,
             label       => $semaphore_label,
         );
         
@@ -157,9 +176,9 @@ sub add_semaphore_node {
             my $dependent_job_node_name = add_job_node( $dependent_job );
 
             $self->{'graph'}->add_edge( $semaphore_node_name => $dependent_job_node_name,
-                color       => $dependent_blocking_colour,
+                color       => $dependent_blocking_arrow_colour,
                 style       => 'dashed',
-                arrowhead   => 'tee',
+                arrowhead   => $dependent_blocking_arrow_shape,
             );
 
                 # adding the semaphore node to the dependent job's cluster:
@@ -191,12 +210,14 @@ sub add_family_tree {
         my $semaphore_node_name = add_semaphore_node( $controlled_semaphore );
 
         my $parent_status               = $parent_job->status;
-        my $parent_controlling_colour   = ($parent_status eq 'DONE' or $parent_status eq 'PASSED_ON') ? 'grey' : 'red';
+        my $parent_is_blocking          = ($parent_status eq 'DONE' or $parent_status eq 'PASSED_ON') ? 0 : 1;
+        my $parent_controlling_colour   = $parent_is_blocking ? 'red' : 'darkgreen';
+        my $blocking_arrow              = $parent_is_blocking ? 'tee' : 'none';
 
         $self->{'graph'}->add_edge( $parent_node_name => $semaphore_node_name,
             color       => $parent_controlling_colour,
             style       => 'dashed',
-            arrowhead   => 'tee',
+            arrowhead   => $blocking_arrow,
         );
     }
 
