eb379b6f1f2b7bee6927452db1b58850e969d5ab
Matthieu Muffato <muffato@ebi.ac.uk>
2017-07-19 22:45:48 +0100

    Always favour the current checkout if possible

--- docs/user_manual/xhive.py
+++ docs/user_manual/xhive.py
@@ -103,10 +103,13 @@ hive_colours = {}
 def hive_setup_if_needed():
     if os.environ.get("READTHEDOCS", None) == "True":
         subprocess.call([os.environ["PWD"] + os.path.sep + "rtd_upgrade.sh"], stdout=sys.stdout, stderr=sys.stderr)
-        os.environ["EHIVE_ROOT_DIR"] = os.path.join(os.environ["PWD"], os.path.pardir, os.path.pardir)
         os.environ["PERL5LIB"] = os.path.pathsep.join(os.path.join(os.environ["HOME"], "packages", _) for _ in ["usr/share/perl5/", "usr/lib/x86_64-linux-gnu/perl5/5.22/", "usr/lib/x86_64-linux-gnu/perl5/5.22/auto/"])
         os.environ["PATH"] = os.path.join(os.environ["HOME"], "packages", "usr/bin") + os.path.pathsep + os.environ["PATH"]
         os.environ["ENSEMBL_CVS_ROOT_DIR"] = os.environ["HOME"]
+    else:
+        os.environ["ENSEMBL_CVS_ROOT_DIR"]   # Will raise an error if missing
+    os.environ["EHIVE_ROOT_DIR"] = os.path.join(os.environ["PWD"], os.path.pardir, os.path.pardir)
+    os.environ["PERL5LIB"] = os.path.join(os.environ["EHIVE_ROOT_DIR"], "modules") + os.path.pathsep + os.environ["PERL5LIB"]
     doxygen_target = os.path.join(os.environ["EHIVE_ROOT_DIR"], "docs", "doxygen")
     if True or any(not os.path.exists(os.path.join(doxygen_target, _)) for _ in ["perl", "python3", "java"]):
         mkdoc_path = os.path.join(os.environ["EHIVE_ROOT_DIR"], "scripts", "dev", "make_docs.pl")
