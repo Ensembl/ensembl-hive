cc7a99f62db613a4323240f571aa6016186c51c9
Matthieu Muffato <muffato@ebi.ac.uk>
2017-08-25 12:33:20 +0100

    Changed the PDF fonts (following dtinth/rtfd-protips)

--- docs/conf.py
+++ docs/conf.py
@@ -227,7 +227,12 @@ latex_elements = {
 #'pointsize': '10pt',
 
 # Additional stuff for the LaTeX preamble.
-'preamble': '\usepackage{pdflscape}',
+'preamble': r'''
+\usepackage{pdflscape}
+\usepackage{charter}
+\usepackage[defaultsans]{lato}
+\usepackage{inconsolata}
+''',
 }
 
 # Grouping the document tree into LaTeX files. List of tuples
