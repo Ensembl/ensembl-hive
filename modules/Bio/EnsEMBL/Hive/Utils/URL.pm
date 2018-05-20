c939dc2cce863c2be1aa6fcf9f87d366b90dd274
Matthieu Muffato <muffato@ebi.ac.uk>
2018-05-20 12:46:24 +0100

    typo

--- modules/Bio/EnsEMBL/Hive/Utils/URL.pm
+++ modules/Bio/EnsEMBL/Hive/Utils/URL.pm
@@ -270,8 +270,8 @@ sub hide_url_password {
     # NOTE: the environment is propagated to the children, meaning that the
     # variable, once set by beekeeper.pl, will extend to all its workers,
     # which is fine as long as beekeeper protects the passwords.
-    return if $ENV{EHIVE_SANIIZED_ARGS};
-    $ENV{EHIVE_SANIIZED_ARGS} = 1;
+    return if $ENV{EHIVE_SANITIZED_ARGS};
+    $ENV{EHIVE_SANITIZED_ARGS} = 1;
 
     # Work on a copy of @ARGV
     my @args = (@ARGV);
