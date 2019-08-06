1e57607e585ddf6f511084dffebe658585d4109d
Matthieu Muffato <muffato@ebi.ac.uk>
2019-08-06 11:02:09 +0100

    [GuestLanguage/Java] Factored out the instantiation of the Runnable

--- wrappers/java/src/main/java/org/ensembl/hive/CompileWrapper.java
+++ wrappers/java/src/main/java/org/ensembl/hive/CompileWrapper.java
@@ -17,10 +17,7 @@
 
 package org.ensembl.hive;
 
-import java.lang.reflect.Constructor;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.ensembl.hive.Utils;
 
 /**
  * Main class for running a hive worker in Java
@@ -32,22 +29,12 @@ public class CompileWrapper {
 
 	public static void main(String[] args) throws Exception {
 
-
 		if (args.length != 1) {
 			System.err.println("Usage: java org.ensembl.hive.CompileWrapper <Runnable class>");
 			System.exit(1);
 		}
 
-		Logger log = LoggerFactory.getLogger(CompileWrapper.class);
-		log.info("Instantiating runnable module " + args[0]);
-		Class<?> clazz = Class.forName(args[0]);
-		if (!BaseRunnable.class.isAssignableFrom(clazz)) {
-			log.error("Class " + args[0] + " must extend "
-					+ BaseRunnable.class.getName());
-			System.exit(2);
-		}
-		Constructor<?> ctor = clazz.getConstructor();
-		BaseRunnable runnable = (BaseRunnable) (ctor.newInstance());
+		Utils.findRunnable(args[0]);
 	}
 
 }
