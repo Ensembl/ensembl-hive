4e1cda63de6a2857886602fcb5429dc43e4f45e2
Matthieu Muffato <muffato@ebi.ac.uk>
2019-08-07 12:53:05 +0100

    [Docker] To save space don't install some of the recommended packages

--- docker/setup_os.Ubuntu-16.04.sh
+++ docker/setup_os.Ubuntu-16.04.sh
@@ -18,11 +18,13 @@
 set -e
 
 apt-get update -y
-apt-get install -y curl perl-doc \
+
+apt-get install -y libgraphviz-perl libchart-gnuplot-perl
+apt-get install -y --no-install-recommends curl perl-doc \
                    sqlite3 libdbd-sqlite3-perl postgresql-client libdbd-pg-perl mysql-client libdbd-mysql-perl libdbi-perl \
                    libcapture-tiny-perl libdatetime-perl libhtml-parser-perl libjson-perl libemail-mime-perl libemail-sender-perl libemail-simple-perl libproc-daemon-perl \
-                   libtest-exception-perl libtest-simple-perl libtest-warn-perl libtest-warnings-perl libtest-file-contents-perl libtest-perl-critic-perl libtest-fatal-perl libgraphviz-perl \
-                   libgetopt-argvfile-perl libchart-gnuplot-perl libbsd-resource-perl
+                   libtest-exception-perl libtest-simple-perl libtest-warn-perl libtest-warnings-perl libtest-file-contents-perl libtest-perl-critic-perl libtest-fatal-perl \
+                   libgetopt-argvfile-perl libbsd-resource-perl
 
 ## Useful for debugging
 #apt-get install -y netcat.openbsd vim perl-doc iputils-ping net-tools apt-file
