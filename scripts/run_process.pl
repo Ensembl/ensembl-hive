e0590a4d1e90e299d77979f2ca6d899f9b042a17
TamaraNaboulsi <ten@ebi.ac.uk>
2022-08-02 12:30:11 +0100

    Adding license lines

--- scripts/run_process.pl
+++ scripts/run_process.pl
@@ -52,3 +52,34 @@ sub parse_options {
   return $params;
 }
 
+__DATA__
+
+=pod
+
+=head1 NAME
+
+run_process.pl
+
+=head1 SYNOPSIS
+
+    run_process.pl -class=<module_name> [<options_for_the_particular_module>]
+
+=head1 DESCRIPTION
+
+run_process.pl is a generic script that is used to call runnables from a Nextflow .nf file. This script initializes the module object and runs the life cycle of that module: fetch_input(), run(), and write_output()
+
+=head1 LICENSE
+
+    Copyright [1999-2015] Wellcome Trust Sanger Institute and the EMBL-European Bioinformatics Institute
+    Copyright [2016-2022] EMBL-European Bioinformatics Institute
+
+    Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+         http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software distributed under the License
+    is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and limitations under the License.
+
+=cut
