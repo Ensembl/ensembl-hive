b0da4b604108c9ee010fe98387099dae872e5490
Arne Becker <arne@ebi.ac.uk>
2024-09-24 16:02:46 +0100

    Update read the docs config
    - sphinx_rtd_theme must now be installed separately

--- docs/conf.py
+++ docs/conf.py
@@ -29,7 +29,6 @@
 import sys
 import os
 import datetime
-import sphinx_rtd_theme
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
@@ -48,6 +47,7 @@ from xhive import setup_if_needed
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'sphinx_rtd_theme',
     'sphinx.ext.autodoc',
     'sphinx.ext.coverage',
     'sphinx.ext.imgmath',
